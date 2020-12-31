d4a0beb0bdaba626c8fbf7c8bd5cb9534545c056
avisilber88 <avisilber88@gmail.com>
2020-12-31 01:37:47 -0500

    checkrhythm
    updated it to matchup with the new system

--- Midi/checkrhythm.js
+++ Midi/checkrhythm.js
@@ -9,29 +9,31 @@ var ourLevelNumber;
 var auth = function () {
 	console.log("hello");
     // alert ("auth");
-    firebase.auth().signInAnonymously()
+	let email="nwhschemscores@gmail.com";
+	let password="nwhschem";
+    firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function (result) {
         db = firebase.firestore();
         db.settings({
             timestampsInSnapshots: true
         });
 
-        db.collection("rhythmsenselogins").get().then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-                // clone template row and append to table body
-                // var tr = tempTr.clone();
-                // tr.data('id', doc.id);
-                // console.warn(doc.id + "");
-                loadDatabase.push(doc.id + "");
-                // var data = doc.data();
-                // // set cell values from Contact data
-                // tr.find('td[data-prop]').each(function () {
-                // var td = $(this);
-                // td.text(data[td.data('prop')] || '');
-                // });
-                // tblBody.append(tr);
-            });
-        });
+        // db.collection("rhythmsenselogins").get().then(function (querySnapshot) {
+            // querySnapshot.forEach(function (doc) {
+                // // clone template row and append to table body
+                // // var tr = tempTr.clone();
+                // // tr.data('id', doc.id);
+                // // console.warn(doc.id + "");
+                // loadDatabase.push(doc.id + "");
+                // // var data = doc.data();
+                // // // set cell values from Contact data
+                // // tr.find('td[data-prop]').each(function () {
+                // // var td = $(this);
+                // // td.text(data[td.data('prop')] || '');
+                // // });
+                // // tblBody.append(tr);
+            // });
+        // });
     })
     .catch(function (error) {
         alert("failed to anonymously sign-in");
@@ -64,12 +66,14 @@ nameis=prompt("name?");
             //document.getElementById('welcomefam').innerHTML = 'Welcome to Harmony Sense, ' + nameis + '!<br><br>Select your level from above, or select freestyle to open up all of the options and settings (how it used to look before).';
        let levelNumber = 0;
         // alert (loadDatabase.length);
-        for (var loadlistplace = 0; loadlistplace < loadDatabase.length; loadlistplace++) {
+        // for (var loadlistplace = 0; loadlistplace < loadDatabase.length; loadlistplace++) {
                     //console.log ("hi")// we got in here
-					db.collection("rhythmsenselogins").doc(loadDatabase[loadlistplace]).get().then(function (doc) {
-       
-                   if (doc.exists) {
-					console.log ("hi")// we got in here
+					db.collection("rhythmsenselogins").where("name", "==", whoItIsUsingThis).orderBy("levelcomplete", "desc").limit(1).get().then(function(querySnapshot) {
+					querySnapshot.forEach(function(doc) {
+            // db.collection("rhythmsenselogins").doc(loadDatabase[loadlistplace]).get().then(function (doc) {
+				// console.error(doc.data());
+                if (doc.exists) {
+					// console.log ("hi")// we got in here
                     var data = doc.data();
                     // console.warn(data.email);
                     // console.warn(data.name);
@@ -101,7 +105,7 @@ nameis=prompt("name?");
                 console.error("failed to read contact");
             });
 
-        }
+        });
 		
 });
 
