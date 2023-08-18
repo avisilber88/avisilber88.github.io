2564c6d6ade450bc6f2e5a39af5008f2424ffd9f
avisilber88 <avisilber88@gmail.com>
2023-08-18 09:04:47 -0400

    update the banner

--- js/custom.js
+++ js/custom.js
@@ -102,9 +102,11 @@ $(document).ready(function() {
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
-       "images/home-bg-slideshow3.jpg",
+       "images/home-bg-slideshow2.jpg", 
+       "images/aob.jpg",
+       "images/testpic4.jpg",
+       "images/testpic3.jpg",
+       "images/testpic6.jpg",
         ],  {duration: 2000, fade: 750});
     });
   })
