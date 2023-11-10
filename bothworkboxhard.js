35b5e8a112dcc750622e049f969965932c1f0cd0
avisilber88 <avisilber88@gmail.com>
2023-11-10 14:00:11 -0500

    Update bothworkboxhard.js

--- bothworkboxhard.js
+++ bothworkboxhard.js
@@ -11,16 +11,33 @@ var givenAnswer;
 var	finalNum = 0;
 var thisisanswer = 0;
 var numberOfSigFigsToCount =0;
+
 //beginning of the things to replace
 	var whatnameis = ""// prompt ("What is your name?");
+
+
+var endSpace="";
+
 	var askagain = function (whatnameis){
 	whatnameis = prompt ("What is your full name (first and last)?");
+	whatnameis=whatnameis.replace(/\s+/g, ' ');
+	// console.error(whatnameis);
+	// console.log(whatnameis.slice(0, 1));
+	// alert("hello"+whatnameis);
+	if (whatnameis.slice(0, 1)==' '){
+	whatnameis=whatnameis.slice(1);
+	}
+	endSpace = whatnameis.slice(-1);
 	if (whatnameis.length<2){
 		askagain();
 	}
 	else if (!(/\s/.test(whatnameis))) {
     // It has any kind of whitespace
-		askagain()
+		askagain();
+	}
+	else if(endSpace==" ") {
+        console.log("ends with space");
+		askagain();
 	}
 	else{
 	document.getElementById("nameis").innerHTML = whatnameis;
